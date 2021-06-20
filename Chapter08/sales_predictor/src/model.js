740627f705895cf6cfd003b05bede1911552fca9
Rising Odegua <risingodegua@gmail.com>
2021-06-20 18:03:00 +0100

    Update model.js

--- Chapter08/sales_predictor/src/model.js
+++ Chapter08/sales_predictor/src/model.js
@@ -3,12 +3,11 @@ const tf = dfd.tf
 
 const LEARNING_RATE = 0.01
 
-function get_model() {
+function getModel() {
     const model = tf.sequential();
-    model.add(tf.layers.dense({ inputShape: [10], units: 128, kernelInitializer: 'leCunNormal' }));
+    model.add(tf.layers.dense({ inputShape: [11], units: 128, kernelInitializer: 'leCunNormal' }));
     model.add(tf.layers.dense({ units: 64, activation: 'relu' }));
     model.add(tf.layers.dense({ units: 32, activation: 'relu' }));
-    model.add(tf.layers.dense({ units: 24, activation: 'relu' }));
     model.add(tf.layers.dense({ units: 1 }))
 
     model.compile({
@@ -17,7 +16,9 @@ function get_model() {
         metrics: ['mse']
     });
 
+    model.summary()
     return model
 }
 
-module.exports = { get_model }
+
+module.exports = { getModel }
