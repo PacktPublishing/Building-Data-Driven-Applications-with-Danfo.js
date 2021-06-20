4da341464d7fd676767417ab7401bcd4011e6b18
Rising Odegua <risingodegua@gmail.com>
2021-06-20 18:03:48 +0100

    Update data-proc.js

--- Chapter08/sales_predictor/src/data-proc.js
+++ Chapter08/sales_predictor/src/data-proc.js
@@ -2,8 +2,10 @@ const dfd = require("danfojs-node")
 
 async function processData(trainDataPath) {
     const salesDf = await dfd.read_csv(trainDataPath)
+    // salesDf.head().print()
 
-    //fill missing columns
+    //Check and fill missing columns
+    // salesDf.isna().sum().print()
     salesDf.fillna({
         columns: ["Item_Weight", "Outlet_Size"],
         values: [salesDf['Item_Weight'].mean(), "Medium"],
@@ -12,7 +14,7 @@ async function processData(trainDataPath) {
 
     //label encode categorical feature
     let encoder = new dfd.LabelEncoder()
-    let catCols = salesDf.select_dtypes(includes = ['string']).column_names
+    let catCols = salesDf.select_dtypes(includes = ['string']).column_names // get all categorical column names
     catCols.forEach(col => {
         encoder.fit(salesDf[col])
         enc_val = encoder.transform(salesDf[col])
@@ -21,7 +23,7 @@ async function processData(trainDataPath) {
 
     //split data into train and target values
     let Xtrain, ytrain;
-    Xtrain = salesDf.iloc({ columns: [`1:${salesDf.columns.length - 1}`] })
+    Xtrain = salesDf.iloc({ columns: [`:${salesDf.columns.length - 1}`] })
     ytrain = salesDf['Item_Outlet_Sales']
     console.log(`Training Dataset Shape: ${Xtrain.shape}`)
 
@@ -30,7 +32,6 @@ async function processData(trainDataPath) {
     scaler.fit(Xtrain)
     Xtrain = scaler.transform(Xtrain)
 
-    //return final tensors for training
     return [Xtrain.tensor, ytrain.tensor]
 }
 
