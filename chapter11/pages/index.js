f7b75585b90f5d00bb56f14db63953d3c57b6aa0
steveoni <steohenoni2@gmail.com>
2021-07-11 12:13:06 +0000

    copy the code from test.js to index.js , and delete test.js

--- chapter11/pages/index.js
+++ chapter11/pages/index.js
@@ -1,82 +1,125 @@
 import Head from 'next/head'
+import React, { useRef, useState } from 'react'
+import dynamic from 'next/dynamic'
+import Plot from '../components/Plot'
 
+const DynamicValueCounts = dynamic(
+  () => import('../components/ValueCounts'),
+  { ssr: false }
+)
+
+const Table = dynamic(
+  () => import('../components/Table'),
+  { ssr: false }
+)
 export default function Home() {
+  let [data, setData] = useState()
+  let [user, setUser] = useState()
+  let [dataNlp, setDataNlp] = useState()
+
+  let inputRef = useRef()
+  const handleSubmit = async () => {
+    const res = await fetch(
+      '/api/tweet',
+      {
+        body: JSON.stringify({
+          username: inputRef.current.value
+        }),
+        headers: {
+          'Content-Type': 'application/json'
+        },
+        method: 'POST'
+      }
+    )
+    const result = await res.json()
+
+    const resSentiment = await fetch(
+      '/api/nlp',
+      {
+        body: JSON.stringify({
+          username: inputRef.current.value,
+          dfData: result
+        }),
+        headers: {
+          'Content-Type': 'application/json'
+        },
+        method: 'POST'
+      },
+
+    )
+    const sentData = await resSentiment.json()
+    setDataNlp(sentData)
+    setUser(inputRef.current.value)
+    setData(result)
+  }
+  const handleKeyEvent = async (event) => {
+    if (event.key === 'Enter') {
+      await handleSubmit()
+    }
+  }
   return (
     <div className="flex flex-col items-center justify-center min-h-screen py-2">
       <Head>
-        <title>Create Next App</title>
+        <title>Twitter Platform</title>
         <link rel="icon" href="/favicon.ico" />
+        <script src="https://cdn.plot.ly/plotly-2.2.0.min.js"></script>
       </Head>
 
       <main className="flex flex-col items-center justify-center w-full flex-1 px-20 text-center">
         <h1 className="text-6xl font-bold">
           Welcome to{' '}
-          <a className="text-blue-600" href="https://nextjs.org">
-            Next.js!
-          </a>
+          <span className="text-blue-600">
+            Twitter Dashboard!
+          </span>
         </h1>
 
-        <p className="mt-3 text-2xl">
-          Get started by editing{' '}
-          <code className="p-3 font-mono text-lg bg-gray-100 rounded-md">
-            pages/index.js
-          </code>
-        </p>
+        <div className='border-2 flex justify-between p-2 rounded-md  md:p-4'>
+          <input id='searchInput' type='text' placeholder='Search twitter user' className='focus:outline-none' ref={inputRef} onKeyPress={handleKeyEvent} />
+          <button className='focus:outline-none' onClick={() => { handleSubmit() }}><img src="/search.svg" /></button>
+        </div>
 
         <div className="flex flex-wrap items-center justify-around max-w-4xl mt-6 sm:w-full">
-          <a
-            href="https://nextjs.org/docs"
-            className="p-6 mt-6 text-left border w-96 rounded-xl hover:text-blue-600 focus:text-blue-600"
+          <div
+            className="p-6 mt-6 text-left border w-96 rounded-xl"
           >
-            <h3 className="text-2xl font-bold">Documentation &rarr;</h3>
+            <h3 className="text-2xl font-bold">Tweet source</h3>
             <p className="mt-4 text-xl">
-              Find in-depth information about Next.js features and API.
+              Display the device for all the tweet user is mentioned in.
             </p>
-          </a>
+            {typeof data != "undefined" && <DynamicValueCounts data={data} column={"source"} type={"PieChart"} />}
+          </div>
 
-          <a
-            href="https://nextjs.org/learn"
+          <div
             className="p-6 mt-6 text-left border w-96 rounded-xl hover:text-blue-600 focus:text-blue-600"
           >
-            <h3 className="text-2xl font-bold">Learn &rarr;</h3>
+            <h3 className="text-2xl font-bold">User Interactions</h3>
             <p className="mt-4 text-xl">
-              Learn about Next.js in an interactive course with quizzes!
+              A chart of users that interact with the given twitter username.
             </p>
-          </a>
+            {typeof data != "undefined" && <DynamicValueCounts data={data} column={"users"} username={user} type={"BarChart"} />}
+          </div>
 
-          <a
-            href="https://github.com/vercel/next.js/tree/master/examples"
-            className="p-6 mt-6 text-left border w-96 rounded-xl hover:text-blue-600 focus:text-blue-600"
+          <div
+            className="p-6 mt-6 text-left border w-96 rounded-xl"
           >
-            <h3 className="text-2xl font-bold">Examples &rarr;</h3>
+            <h3 className="text-2xl font-bold">Sentiment Analysis</h3>
             <p className="mt-4 text-xl">
-              Discover and deploy boilerplate example Next.js projects.
+              A chart showing the overall sentiment percentage for all the tweet.
             </p>
-          </a>
+            {typeof data != "undefined" && <Plot data={dataNlp} />}
+          </div>
 
-          <a
-            href="https://vercel.com/import?filter=next.js&utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-            className="p-6 mt-6 text-left border w-96 rounded-xl hover:text-blue-600 focus:text-blue-600"
+          <div
+            className="p-6 mt-6 text-left border w-96 rounded-xl"
           >
-            <h3 className="text-2xl font-bold">Deploy &rarr;</h3>
+            <h3 className="text-2xl font-bold">User Data</h3>
             <p className="mt-4 text-xl">
-              Instantly deploy your Next.js site to a public URL with Vercel.
+              A table showing the extracted data for the given twitter username.
             </p>
-          </a>
+            {typeof data != "undefined" && <Table dfData={data} username={user} />}
+          </div>
         </div>
       </main>
-
-      <footer className="flex items-center justify-center w-full h-24 border-t">
-        <a
-          className="flex items-center justify-center"
-          href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Powered by{' '}
-          <img src="/vercel.svg" alt="Vercel Logo" className="h-4 ml-2" />
-        </a>
-      </footer>
     </div>
   )
 }
